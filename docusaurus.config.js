c8ecd33161d39f0e4664e33170f61324cb727505
Felix Brockmeier <felix.brockmeier@outlook.com>
2022-08-26 18:56:54 +0200

    google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,6 +36,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-DGNP2T84RK',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
